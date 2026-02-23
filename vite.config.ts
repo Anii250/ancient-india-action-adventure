2d46584c409112f7dc7d7f87850c7a1ed51fbe96
asus <asus@example.com>
2026-02-23 18:26:57 +0530

    Fix vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/ancient-india-action-adventure/",
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
